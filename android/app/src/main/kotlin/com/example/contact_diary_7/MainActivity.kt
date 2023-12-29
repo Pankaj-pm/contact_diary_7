81b06128394137d32fa0923d46afd0d2c1805966
AnandDL02 <ananddalsaniya2002@gmail.com>
2023-12-29 10:16:47 +0530

    platform change

--- android/app/src/main/kotlin/com/example/contact_diary_7/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/contact_diary_7/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.contact_diary_7
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:contact_diary_7/add_contact_page.dart';
 import 'package:contact_diary_7/add_contact_provider.dart';
 import 'package:contact_diary_7/contact_provider.dart';
 import 'package:contact_diary_7/theme_provider.dart';
 import 'package:contact_diary_7/util.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -15,6 +20,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final LocalAuthentication auth = LocalAuthentication();
+
   @override
   Widget build(BuildContext context) {
     print("Home Page");
@@ -40,37 +47,51 @@ class _HomePageState extends State<HomePage> {
             var contactModel = contactListProvider.contactList[index];
             return ListTile(
               title: Text(contactModel.name ?? ""),
+              splashColor: Colors.black12,
+              onTap: () {
+
+              },
               subtitle: Text(contactModel.number ?? ""),
               trailing: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  IconButton(
-                    onPressed: () {
-                      Provider.of<ContactListProvider>(context, listen: false).remove(index);
-                    },
-                    icon: Icon(Icons.delete),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return AddContactPage(
-                            index: index,
-                          );
-                        },
-                      ));
-                    },
-                    icon: Icon(Icons.edit),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      // var uri = Uri.parse("tel:${contactModel.number ?? ""}");
-                      var uri = Uri.parse("https://flutter.dev");
+                  PopupMenuButton(
+                    position: PopupMenuPosition.under,
+                    // padding: EdgeInsetsDirectional.symmetric(vertical: 150),
+                    clipBehavior: Clip.antiAlias,
+                    icon: Icon(Icons.add),
+                    itemBuilder:(context) {
+                      return [
+                        PopupMenuItem(child:  IconButton(
+                          onPressed: () {
+                            // var uri = Uri.parse("tel:${contactModel.number ?? ""}");
+                            var uri = Uri.parse("https://api.whatsapp.com/send/?phone=918401784759&text&type=phone_number&app_absent=0");
 
-                      launchUrl(uri);
+                            launchUrl(uri);
+                          },
+                          icon: Icon(Icons.call),
+                        ),),
+                        PopupMenuItem(child: IconButton(
+                          onPressed: () {
+                            Provider.of<ContactListProvider>(context, listen: false).remove(index);
+                          },
+                          icon: Icon(Icons.delete),
+                        ),),
+                        PopupMenuItem(child: IconButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(
+                              builder: (context) {
+                                return AddContactPage(
+                                  index: index,
+                                );
+                              },
+                            ));
+                          },
+                          icon: Icon(Icons.edit),
+                        ),),
+                      ];
                     },
-                    icon: Icon(Icons.call),
-                  ),
+                  )
                 ],
               ),
             );
@@ -79,12 +100,21 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AddContactPage(),
-              ));
-          setState(() {});
+
+            print("isAndroid ${Platform.isAndroid}");
+            print("isFuchsia ${Platform.isFuchsia}");
+            print("isIOS ${Platform.isIOS}");
+            print("isLinux ${Platform.isLinux}");
+            print("isMacOS ${Platform.isMacOS}");
+            print("isWindows ${Platform.isWindows}");
+            print("isWeb ${kIsWeb}");
+
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //       builder: (context) => AddContactPage(),
+          //     ));
+          // setState(() {});
         },
         child: const Icon(Icons.add),
       ),
